cf101762a16a1210da7f4a09ce6e508a0aa126e6
Ekaterina <88279403+EkaterinaT89@users.noreply.github.com>
2021-10-01 10:47:25 +0300

    Update GraffitiAttachments.kt

--- src/main/kotlin/GraffitiAttachments.kt
+++ src/main/kotlin/GraffitiAttachments.kt
@@ -1,18 +1,4 @@
-class GraffitiAttachments (
+class GraffitiAttachments : Attachment {
+    override val type: String = "graffiti"
     val graffiti: Graffiti = Graffiti()
-
-): Attachments {
-    override var type: String = "graffiti"
-        set(value) {
-            if (type == "graffiti") {
-                type as Video
-            }
-            field = value
-        }
-        get() = field
-
-    override fun toString(): String {
-        return "$type" + graffiti.toString()
-    }
-
 }

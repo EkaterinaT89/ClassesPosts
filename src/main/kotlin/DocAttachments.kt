befd45f907e87caa06db59c7f8b1fb3eafe19ee7
Ekaterina <88279403+EkaterinaT89@users.noreply.github.com>
2021-10-01 10:46:41 +0300

    Update DocAttachments.kt

--- src/main/kotlin/DocAttachments.kt
+++ src/main/kotlin/DocAttachments.kt
@@ -1,19 +1,4 @@
-class DocAttachments (
+class DocAttachments : Attachment {
+    override val type: String = "doc"
     val doc: Doc = Doc()
-
-): Attachments {
-
-    override var type: String = "doc"
-        set(value) {
-            if (type == "doc") {
-                type as Doc
-            }
-            field = value
-        }
-        get() = field
-
-    override fun toString(): String {
-        return "$type" + doc.toString()
-    }
-
 }
